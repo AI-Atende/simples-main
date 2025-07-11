46ae099a0a153fe79f54c8ece649057376ddb2f8
alan matheus <matheusalan334@gmail.com>
2025-07-11 17:07:05 -0300

    videoata

--- src/components/SectionVideos.tsx
+++ src/components/SectionVideos.tsx
@@ -5,6 +5,9 @@ import { useKeenSlider } from "keen-slider/react";
 import "keen-slider/keen-slider.min.css";
 import { IoIosArrowForward } from "react-icons/io";
 import { MdArrowBackIosNew } from "react-icons/md";
+import video01 from "../assets/imgs/clincheck1.mp4";
+import video02 from "../assets/imgs/clincheck2.mp4";
+import video03 from "../assets/imgs/ClinCheck4.mp4";
 
 const SectionVideos = () => {
   const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
@@ -17,25 +20,42 @@ const SectionVideos = () => {
 
   const videos = [
     {
-      src: "https://www.w3schools.com/html/mov_bbb.mp4",
-      titulo:"Veja em 3D como seu novo sorriso vai ficar",
-description: ``,
-description2: "Veja em 3D como seu novo sorriso vai ficar",    
-},
-    {
-      src: "https://www.w3schools.com/html/movie.mp4",
-      description: "Vídeo explicativo sobre as funcionalidades da plataforma.",
+      src: video01,
+      titulo: "Veja em 3D como seu novo sorriso vai ficar",
+      description:
+        "Com a tecnologia exclusiva ClinCheck®, você consegue visualizar todo o planejamento do seu tratamento Invisalign antes mesmo de começar. ",
+      description2:
+        "No vídeo ao lado, mostramos uma simulação real: é possível ver o movimento exato de cada dente, etapa por etapa, até alcançar o alinhamento ideal.",
     },
     {
-      src: "https://www.w3schools.com/html/mov_bbb.mp4",
-      description: "Exemplo de interação real com o sistema apresentado.",
+      src: video02,
+      titulo: "Acompanhamento Digital: Veja a Evolução do Seu Sorriso",
+      description:
+        "Neste vídeo, você confere uma simulação completa do tratamento com alinhadores Invisalign usando a tecnologia ClinCheck®.",
+      description2:
+        "O destaque aqui é a evolução detalhada do alinhamento dentário, mostrando como cada dente se movimenta ao longo das semanas.",
+    },
+    {
+      src: video03,
+      titulo: "Precisão Milimétrica no Planejamento do Seu Sorriso",
+      description:
+        "O vídeo acima mostra o poder do ClinCheck®, o sistema de inteligência 3D usado no tratamento com alinhadores Invisalign.",
+      description2:
+        "Com ele, nossos especialistas conseguem planejar cada movimento com precisão milimétrica — seja para girar um dente, alinhar a mordida ou corrigir a inclinação.",
     },
   ];
 
   return (
-    <section className="py-10 px-4 w-auto bg-blue-100">
-      <h2 className="text-2xl font-bold w-100 text-center mb-6">Assista Nossos Vídeos</h2>
-
+    <section className="py-10 px-4 w-auto bg-white-100">
+      <div className="text-center">
+        <h2 className="text-4xl font-bold text-gray-900 mb-4">
+          Veja Seu Novo Sorriso Antes Mesmo de Sorrir para o Espelho
+        </h2>
+        <p className="text-xl text-gray-600 max-w-3xl mx-auto">
+          Com o planejamento digital 3D do ClinCheck®, você acompanha cada etapa
+          da sua transformação com segurança, clareza e emoção.
+        </p>
+      </div>
       <div className="relative">
         {/* Setas laterais */}
         <button
@@ -53,13 +73,19 @@ description2: "Veja em 3D como seu novo sorriso vai ficar",
                   src={video.src}
                   controls
                   className="rounded-md w-2/4 h-100 object-cover mb-4"
+                  autoPlay
+                  muted
+                  playsInline
                 />
-                <div className="text-wrap">
-                  <h2>{video.titulo}</h2>
-                <p className="text-gray-700  ml-20 text-">{video.description}</p>
-                <br />
-                <p className="text-gray-700  ml-20 text-">{video.description2}</p>
-              </div>
+                <div className="text-wrap  ml-10">
+                  <h2 className="text-xl mb-5 ">{video.titulo}</h2>
+                  <p className="text-gray-700 text-[15px] w-[500px]">
+                    {video.description}
+                  </p>
+                  <p className="text-gray-700 text-[15px] w-[500px]">
+                    {video.description2}
+                  </p>
+                </div>
               </div>
             </div>
           ))}
